e7a12bd78f81868634df4256f1af40670684d8eb
Philipp Lypniakov <filipprayden@gmail.com>
2019-01-07 22:14:57 +0200

    database update

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host: '127.0.0.1',
+    host: 'postgresql-fitted-67203',
     user: 'philipp',
     password: '123',
     database: 'smart-brain'
